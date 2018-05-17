857d080952ab19b37572d50400f6203fea9681d8
Alexandre Girard <git@alexgirard.com>
2018-05-17 12:53:17 +0200

    feat: add overlay on About header menu #2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { inject, observer } from 'mobx-react';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+
+import AboutTooltip from './AboutTooltip';
 
 @inject('commonStore')
 @observer
 class Header extends React.Component {
+
   render() {
+
+    const tooltip = (<Tooltip id='tooltip'>
+      <p>core-ui
+        <ul>
+          <li>commit : f60a79</li>
+        </ul>
+      </p>
+      <p>deepdetect
+        <ul>
+          <li>version : 0.1</li>
+          <li>commit : e8592d</li>
+        </ul>
+      </p>
+    </Tooltip>);
+
     return (
-      <header className="header navbar navbar-dark bg-dark">
+      <header className="header navbar navbar-dark bg-dark" id='header'>
         <div className="container-fluid">
           <div className="header-content">
 
@@ -20,15 +39,19 @@ class Header extends React.Component {
 
               <ul className="list-unstyled navbar-sub-nav">
                 <li>
-                  <Link to="/" className="nav-link">
-                    About
-                  </Link>
+                  <OverlayTrigger placement='bottom' overlay={tooltip}>
+										<a className='nav-link'>
+											About
+										</a>
+									</OverlayTrigger>
                 </li>
               </ul>
+
             </div>
 
           </div>
         </div>
+
       </header>
     );
   }
