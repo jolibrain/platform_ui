d854879c2f4a6c6e80db72ee4a7ad73f95cfa8ea
Alexandre Girard <git@alexgirard.com>
2018-05-21 10:34:28 +0200

    refactor: review GpuInfo widget style

--- src/components/widgets/GpuInfo.js
+++ src/components/widgets/GpuInfo.js
@@ -18,7 +18,7 @@ export default class GpuInfo extends React.Component {
   }
 
   componentDidMount() {
-    var intervalId = setInterval(this.timer.bind(this), 1000);
+    var intervalId = setInterval(this.timer.bind(this), 5000);
     this.setState({intervalId: intervalId});
   }
 
@@ -41,20 +41,39 @@ export default class GpuInfo extends React.Component {
         {
           gpuInfo.gpus.map( (gpu, index) => {
 
-            const memoryPercent = parseInt(gpu['memory.used'] * 100 / gpu['memory.total'], 10);
+            const memoryMo = parseInt(gpu['memory.used'], 10);
+            const memoryPercent = parseInt(memoryMo * 100 / gpu['memory.total'], 10);
             const utilPercent = parseInt(gpu['utilization.gpu'], 10);
 
             return (
               <div key={`gpuInfo${index}`} className="block">
+
                 <div className="context-header">
                   <div className="sidebar-context-title">GPU {index}</div>
                 </div>
-                <div className="progress">
-                  <div className="progress-bar" role="progressbar" style={{width: `${memoryPercent}%`}} aria-valuenow={memoryPercent} aria-valuemin="0" aria-valuemax="100">Memory {index}</div>
+
+                <div className='row'>
+                  <div className='col-md-2'>
+                    Mem.:
+                  </div>
+                  <div className='col-md-10'>
+                    <div className="progress">
+                      <div className="progress-bar" role="progressbar" style={{width: `${memoryPercent}%`}} aria-valuenow={memoryPercent} aria-valuemin="0" aria-valuemax="100">{memoryMo}Mo</div>
+                    </div>
+                  </div>
                 </div>
-                <div className="progress">
-                  <div className="progress-bar" role="progressbar" style={{width: `${utilPercent}%`}} aria-valuenow={utilPercent} aria-valuemin="0" aria-valuemax="100">Util. {index}</div>
+
+                <div className='row'>
+                  <div className='col-md-2'>
+                    Util.:
+                  </div>
+                  <div className='col-md-10'>
+                    <div className="progress">
+                      <div className="progress-bar" role="progressbar" style={{width: `${utilPercent}%`}} aria-valuenow={utilPercent} aria-valuemin="0" aria-valuemax="100">{utilPercent}%</div>
+                    </div>
+                  </div>
                 </div>
+
                 <div className="list">
                   <ul>
                   {
