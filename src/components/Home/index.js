06cde8dc01ff6143bc394936bc201aace9c9ebff
Alexandre Girard <git@alexgirard.com>
2018-05-25 11:41:50 +0200

    fix: review Home display - fix #19

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,40 +1,19 @@
+import LeftPanel from './LeftPanel';
+import MainView from './MainView';
 import React from 'react';
-import { Timeline } from 'react-twitter-widgets'
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-
-import ServiceList from '../widgets/ServiceList';
-import GpuInfo from '../widgets/GpuInfo';
 
 export default class Home extends React.Component {
 
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <div className="container home">
-        <div className="row">
+      <div className="layout-page page-gutter page-with-contextual-sidebar right-sidebar-collapsed page-with-icon-sidebar home-component">
 
-          <div className="col-md-4">
-            <h5><FontAwesomeIcon icon="cube"/> Predict</h5>
-            <ServiceList />
-          </div>
-
-          <div className="col-md-4">
-            <GpuInfo />
-          </div>
-
-          <div className="col-md-4">
-            <Timeline
-              dataSource={{
-                sourceType: 'profile',
-                screenName: 'jolibrain'
-              }}
-              options={{
-                username: 'Jolibrain',
-                height: '600'
-              }}
-            />
-          </div>
-
-        </div>
+        <LeftPanel />
+        <MainView />
 
     </div>
     );
