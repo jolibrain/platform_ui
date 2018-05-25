09701e95146c30207815313ef70eeda33539329e
Alexandre Girard <git@alexgirard.com>
2018-05-25 12:43:05 +0200

    feat: add breadcrumbs and navbar header on service - fix #18

--- src/components/Service/MainView.js
+++ src/components/Service/MainView.js
@@ -1,68 +1,31 @@
 import React from 'react';
-import { inject, observer } from 'mobx-react';
 import { Link, withRouter } from 'react-router-dom'
-import { parse as qsParse } from 'query-string';
 
 import RightPanel from './RightPanel';
 import Imaginate from '../widgets/Imaginate';
 
-@inject('commonStore')
 @withRouter
-@observer
 export default class MainView extends React.Component {
 
-  componentWillMount() {
-    //this.props.articlesStore.setPredicate(this.getPredicate());
-  }
-
-  componentDidMount() {
-    //this.props.articlesStore.loadArticles();
-  }
-
-  componentDidUpdate(previousProps) {
-    //if (
-    //  this.getTab(this.props) !== this.getTab(previousProps) ||
-    //  this.getTag(this.props) !== this.getTag(previousProps)
-    //) {
-    //  this.props.articlesStore.setPredicate(this.getPredicate());
-    //  this.props.articlesStore.loadArticles();
-    //}
-  }
-
-  getTag(props = this.props) {
-    return qsParse(props.location.search).tag || "";
-  }
-
-  getTab(props = this.props) {
-    return qsParse(props.location.search).tab || 'all';
-  }
-
-  getPredicate(props = this.props) {
-    switch (this.getTab(props)) {
-      case 'feed': return { myFeed: true };
-      case 'tag': return { tag: qsParse(props.location.search).tag };
-      default: return {};
-    }
-  }
-
-  handleTabChange = (tab) => {
-    if (this.props.location.query.tab === tab) return;
-    this.props.router.push({ ...this.props.location, query: { tab } })
-  };
-
-  handleSetPage = page => {
-    this.props.articlesStore.setPage(page);
-    this.props.articlesStore.loadArticles();
-  };
-
   render() {
+
+    const serviceName = this.props.match.params.serviceName;
+
     return (
       <div className="main-view content-wrapper">
         <div className="container-fluid">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <ul className="nav navbar-nav ml-auto">
+          <div className='breadcrumbs'>
+            <Link to='/'>DeepDetect</Link> >&nbsp;
+            <Link to='/predict'>Predict</Link> >&nbsp;
+            <Link to={`/predict/${serviceName}`}>{serviceName}</Link>
+          </div>
+          <nav className="navbar navbar-expand-lg">
+            <ul className="nav navbar-nav ml-auto" style={{flexDirection: 'row'}}>
               <li className="nav-item">
-                <Link to='/predict/new' className="btn btn-primary">New Service</Link>
+                <Link to={`/predict/${serviceName}/delete`} className="btn btn-outline-danger">Delete Service</Link>
+              </li>
+              <li className="nav-item">
+                <Link to='/predict/new' className="btn btn-outline-primary">New Service</Link>
               </li>
             </ul>
           </nav>
