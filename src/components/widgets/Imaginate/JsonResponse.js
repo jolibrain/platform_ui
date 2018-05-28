0d46d42c80437f3f1b5e0e529acafbc36ae2b5c8
Alexandre Girard <git@alexgirard.com>
2018-05-28 11:07:54 +0200

    feat: add CopytoClipboard on Imaginate Json content

--- src/components/widgets/Imaginate/JsonResponse.js
+++ src/components/widgets/Imaginate/JsonResponse.js
@@ -5,9 +5,28 @@ import {Controlled as CodeMirror} from 'react-codemirror2'
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/mode/javascript/javascript';
 
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
 @inject('imaginateStore')
 @observer
 export default class JsonResponse extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      copied: false,
+    }
+
+    this.handleCopyClipboard = this.handleCopyClipboard.bind(this);
+  }
+
+  handleCopyClipboard() {
+    this.setState({copied: true});
+    setTimeout(() => {
+      this.setState({copied: false})
+    }, 2000);
+  }
 
   render() {
 
@@ -16,11 +35,27 @@ export default class JsonResponse extends React.Component {
     if(store.selectedImage === null)
       return null;
 
+    const jsonContent = store.selectedImage.json;
+
+    let copiedClass = 'btn btn-sm btn-outline-secondary';
+    let copiedText = 'Copy to clipboard';
+
+    if(this.state.copied === true) {
+      copiedClass = 'btn btn-sm btn-outline-success';
+      copiedText = 'Copied';
+    }
+
     return (
       <div>
-        <h6>JSON response</h6>
+        <h6>
+          JSON response&nbsp;
+          <CopyToClipboard text={jsonContent}
+            onCopy={this.handleCopyClipboard}>
+            <button type="button" className={copiedClass}>{copiedText}</button>
+          </CopyToClipboard>
+        </h6>
         <CodeMirror
-          value={JSON.stringify(store.selectedImage.json, null, 1)}
+          value={JSON.stringify(jsonContent, null, 1)}
         />
       </div>
     );
