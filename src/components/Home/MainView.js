5a33ac9b36f35461b349f0f368ee30c5a52a1256
Alexandre Girard <git@alexgirard.com>
2018-05-31 16:18:37 +0200

    fix: add welcome message

--- src/components/Home/MainView.js
+++ src/components/Home/MainView.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { Link, withRouter } from "react-router-dom";
 
 import RightPanel from "./RightPanel";
 
+@withRouter
 export default class MainView extends React.Component {
   render() {
     return (
       <div className="main-view content-wrapper">
         <div className="container-fluid">
           <div className="content">
+            <h2>DeepDetect Platform</h2>
+
+            <p>
+              Welcome to <b>DeepDetect Platform</b>
+            </p>
+            <Link to="/predict/new" className="btn btn-outline-primary">
+              New Service
+            </Link>
             <RightPanel />
           </div>
         </div>
