7baebbc976f8b81032e8abc2b9e226e49bd82318
Alexandre Girard <git@alexgirard.com>
2018-06-07 15:16:30 +0200

    fix: remove unused dependency

--- src/components/widgets/Imaginate/index.js
+++ src/components/widgets/Imaginate/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { inject, observer } from "mobx-react";
 import { withRouter } from "react-router-dom";
-import ReactTooltip from "react-tooltip";
 
 import ImageList from "./ImageList";
 import BoundingBoxDisplay from "./BoundingBoxDisplay";
