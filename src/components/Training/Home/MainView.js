a211f115a318926475ce55fd8cbcc73af002b8e3
Alexandre Girard <git@alexgirard.com>
2018-07-02 14:49:48 +0200

    fix: add message when no training services running

--- src/components/Training/Home/MainView.js
+++ src/components/Training/Home/MainView.js
@@ -1,21 +1,33 @@
 import React from "react";
-import { observer } from "mobx-react";
+import { inject, observer } from "mobx-react";
 import { withRouter } from "react-router-dom";
 
 import RightPanel from "../commons/RightPanel";
 import ServiceCardList from "../../widgets/ServiceCardList";
 
+@inject("deepdetectStore")
 @withRouter
 @observer
 export default class MainView extends React.Component {
   render() {
+    const ddStore = this.props.deepdetectStore;
+    const isEmpty = !ddStore.servers.some(server => {
+      return server.services.some(service => service.settings.training);
+    });
+
     return (
       <div className="main-view content-wrapper">
         <div className="container-fluid">
           <div className="content">
             <div className="serviceList">
-              <h4>Current Training Service</h4>
-              <ServiceCardList onlyTraining={true} />
+              {isEmpty ? (
+                <h4>No training service running</h4>
+              ) : (
+                <div>
+                  <h4>Current Training Service</h4>
+                  <ServiceCardList onlyTraining={true} />
+                </div>
+              )}
             </div>
             <RightPanel />
           </div>
