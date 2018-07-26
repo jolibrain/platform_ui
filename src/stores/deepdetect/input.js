468d758fa5496fb6c7a3c7f913dd2d2fbbb78500
Alexandre Girard <git@alexgirard.com>
2018-07-26 11:11:59 +0200

    feat: add computed access to input values

--- src/stores/deepdetect/input.js
+++ src/stores/deepdetect/input.js
@@ -1,4 +1,4 @@
-import { observable } from "mobx";
+import { observable, computed } from "mobx";
 
 export default class Input {
   @observable isActive = false;
@@ -8,4 +8,40 @@ export default class Input {
   @observable json = null;
   @observable error = false;
   @observable boxes = [];
+
+  @computed
+  get prediction() {
+    let prediction = null;
+
+    if (
+      this.json &&
+      this.json.body &&
+      this.json.body.predictions &&
+      this.json.body.predictions[0]
+    ) {
+      prediction = this.json.body.predictions[0];
+    }
+
+    return prediction;
+  }
+
+  @computed
+  get isCtcOuput() {
+    return (
+      this.postData &&
+      this.postData.parameters &&
+      this.postData.parameters.output &&
+      this.postData.parameters.output.ctc
+    );
+  }
+
+  @computed
+  get isSegmentationInput() {
+    return (
+      this.postData &&
+      this.postData.parameters &&
+      this.postData.parameters.input &&
+      this.postData.parameters.input.segmentation
+    );
+  }
 }
