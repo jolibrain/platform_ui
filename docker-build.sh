e67bf3f095c90c69beaf78781b6403ce6e7c1659
Alexandre Girard <git@alexgirard.com>
2018-07-27 14:30:08 +0200

    docker: add yarn run build in docker-build process

--- docker-build.sh
+++ docker-build.sh
@@ -9,5 +9,6 @@ TAG=$(git rev-parse --verify --short HEAD)
 
 REGISTRY="eris:7750"
 
+yarn run build
 docker build -t ${REGISTRY}/${IMAGE_NAME}:${TAG} -t ${REGISTRY}/${IMAGE_NAME}:latest .
 docker push ${REGISTRY}/${IMAGE_NAME}
