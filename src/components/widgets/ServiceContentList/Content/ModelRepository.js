7b678462d006019818c246ef98a7c663c7d53fe0
Alexandre Girard <git@alexgirard.com>
2018-09-17 15:16:21 +0200

    feat: add ServiceContentList widget component

---
+++ src/components/widgets/ServiceContentList/Content/ModelRepository.js
@@ -0,0 +1,194 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { observer } from "mobx-react";
+import { Link, withRouter } from "react-router-dom";
+
+@withRouter
+@observer
+export default class ModelRepositoryContent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.getValue = this.getValue.bind(this);
+  }
+
+  getValue(attr) {
+    const { service } = this.props;
+    const { measure, measure_hist } = service.jsonMetrics.body;
+
+    let value = "--";
+
+    if (measure) {
+      value = measure[attr];
+    } else if (
+      measure_hist &&
+      measure_hist[`${attr}_hist`] &&
+      measure_hist[`${attr}_hist`].length > 0
+    ) {
+      value =
+        measure_hist[`${attr}_hist`][measure_hist[`${attr}_hist`].length - 1];
+    }
+
+    if (attr !== "remain_time_str" && value && value !== "--") {
+      if (attr === "train_loss") {
+        value = value.toFixed(10);
+      } else {
+        value = value.toFixed(5);
+      }
+    }
+
+    return value;
+  }
+
+  render() {
+    const repository = this.props.service;
+
+    if (!repository) return null;
+
+    const { mltype } = repository.jsonMetrics.body;
+
+    const archiveUrl = `/trainingArchive/${repository.name}`;
+
+    let badges = [];
+
+    badges.push({
+      classNames: "badge badge-secondary",
+      status: mltype
+    });
+
+    let tags = repository.trainingTags;
+    if (tags && tags.length > 0) {
+      tags.forEach(t => {
+        badges.push({
+          classNames: "badge badge-info",
+          status: t
+        });
+      });
+    }
+
+    let info = [
+      {
+        text: "Train Loss",
+        val: this.getValue("train_loss")
+      }
+    ];
+
+    let val_acc, val_accp;
+
+    switch (mltype) {
+      case "segmentation":
+        info.push({
+          text: "Mean IOU",
+          val: this.getValue("meaniou")
+        });
+        break;
+      case "detection":
+        info.push({
+          text: "MAP",
+          val: this.getValue("map")
+        });
+        break;
+      case "ctc":
+        val_acc = this.getValue("acc");
+        val_accp = this.getValue("accp");
+        info.push({
+          text: "Accuracy",
+          val: val_acc || val_accp
+        });
+        break;
+      case "classification":
+        val_acc = this.getValue("acc");
+        val_accp = this.getValue("accp");
+        info.push({
+          text: "Accuracy",
+          val: val_acc || val_accp
+        });
+        info.push({
+          text: "F1",
+          val: this.getValue("f1")
+        });
+
+        info.push({
+          text: "mcll",
+          val: this.getValue("mcll")
+        });
+        break;
+      case "regression":
+        info.push({
+          text: "Eucll",
+          val: this.getValue("eucll")
+        });
+        break;
+      default:
+        break;
+    }
+
+    let bestModelInfo = null;
+    if (repository.bestModel) {
+      bestModelInfo = (
+        <div>
+          <ul>
+            {Object.keys(repository.bestModel).map((k, i) => {
+              const attrTitle =
+                i === 0
+                  ? k.replace(/\b\w/g, l => l.toUpperCase())
+                  : k.toUpperCase();
+              return (
+                <li key={i}>
+                  {attrTitle} [best]: <b>{repository.bestModel[k]}</b>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      );
+    }
+
+    return (
+      <div
+        className="row"
+        style={{ borderTop: "1px solid #ccc", padding: "5px 0" }}
+      >
+        <div className="col-md-4 main-info">
+          <h5>{repository.name}</h5>
+          <span classNames="badges">
+            {badges.map((badge, key) => {
+              return (
+                <span key={key} className={badge.classNames}>
+                  {badge.loading ? (
+                    <i className="fas fa-spinner fa-spin" />
+                  ) : (
+                    ""
+                  )}
+                  {badge.status}
+                </span>
+              );
+            })}
+          </span>
+        </div>
+        <div className="col-md-3 main-values">
+          <ul>
+            {info.map((i, index) => {
+              return (
+                <li key={index}>
+                  {i.text}: {i.breakline ? <br /> : ""}
+                  <b>{i.val}</b>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="col-md-3 main-best">{bestModelInfo}</div>
+        <div className="col-md-2 main-meta">
+          <Link to={archiveUrl} className="btn btn-outline-primary view">
+            <i class="fas fa-search" /> View
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+ModelRepositoryContent.propTypes = {
+  repository: PropTypes.object
+};
---
+++ src/components/widgets/ServiceContentList/index.js
@@ -0,0 +1,54 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { inject, observer } from "mobx-react";
+import { withRouter } from "react-router-dom";
+
+//import PredictContent from "./Content/Predict";
+//import TrainingContent from "./Content/Training";
+import ModelRepositoryContent from "./Content/ModelRepository";
+
+@inject("configStore")
+@withRouter
+@observer
+export default class ServiceContentList extends React.Component {
+  contentArray(services) {
+    const { filterServiceName } = this.props;
+
+    return services
+      .filter(r => {
+        return filterServiceName ? r.name.includes(filterServiceName) : true;
+      })
+      .map((service, index) => {
+        let content = null;
+        if (service.jsonMetrics) {
+          content = <ModelRepositoryContent key={index} service={service} />;
+        } else if (service.settings.training) {
+          //content = <TrainingContent key={index} service={service} />;
+        } else {
+          //content = <PredictContent key={index} service={service} />;
+        }
+        return content;
+      });
+  }
+
+  render() {
+    const { services } = this.props;
+
+    if (
+      !services ||
+      this.props.configStore.isComponentBlacklisted("ServiceContentList")
+    )
+      return null;
+
+    return (
+      <div className="serviceContentList container-fluid">
+        {this.contentArray(services)}
+      </div>
+    );
+  }
+}
+
+ServiceContentList.propTypes = {
+  services: PropTypes.array.isRequired,
+  filterServiceName: PropTypes.string
+};
