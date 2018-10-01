63828a246b4182a32c3bb67ce580c425a046550f
Alexandre Girard <git@alexgirard.com>
2018-10-01 16:10:45 +0200

    fix: input.path, issue was related to error in eris config.json

--- src/stores/deepdetect/service.js
+++ src/stores/deepdetect/service.js
@@ -242,7 +242,7 @@ export default class deepdetectService {
     this.inputs = serverInputs.map(i => {
       let input = new Input();
       input.content = folder.path + i;
-      input.path = systemPath + folder.relativePath + i;
+      input.path = systemPath + folder.path + i;
       return input;
     });
 
