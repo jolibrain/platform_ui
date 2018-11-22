2b7c4dc34aee3cbc171da85ef09f932dca71176a
Alexandre Girard <git@alexgirard.com>
2018-11-22 09:53:17 +0100

    fix: add more UI details on ServerItem in Header

--- src/components/Header/components/ServerItem.js
+++ src/components/Header/components/ServerItem.js
@@ -5,8 +5,26 @@ import ReactTooltip from "react-tooltip";
 class ServerItem extends React.Component {
   render() {
     const { server } = this.props;
+    const { settings } = server;
 
     const tooltipId = `server-${server.name}-status-tooltip`;
+
+    const serverTooltip = (
+      <div className="server-details">
+        <div className="d-flex mb-2">
+          <i className="fas fa-caret-right" /> Host{" "}
+          <span className="ml-auto">{window.location.hostname}</span>
+        </div>
+        <div className="d-flex mb-2">
+          <i className="fas fa-caret-right" /> Port{" "}
+          <span className="ml-auto">{window.location.port}</span>
+        </div>
+        <div className="d-flex mb-2 last">
+          <i className="fas fa-caret-right" /> Path{" "}
+          <span className="ml-auto pl-2">{settings.path}</span>
+        </div>
+      </div>
+    );
 
     return (
       <a
@@ -26,7 +44,7 @@ class ServerItem extends React.Component {
         &nbsp;
         {server.name}
         <ReactTooltip id={tooltipId} place="left" effect="solid">
-          {server.settings.path}
+          {serverTooltip}
         </ReactTooltip>
       </a>
     );
