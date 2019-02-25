787f079332e2d6708b747a8c72736e463c350498
Alexandre Girard <git@alexgirard.com>
2019-02-25 16:24:57 +0100

    fix: docker-build.sh now push to dockerhub by default

--- docker-build.sh
+++ docker-build.sh
@@ -2,13 +2,20 @@
 
 set -ex
 
-PARENT_DIR=$(basename "${PWD%/*}")
 CURRENT_DIR="${PWD##*/}"
 IMAGE_NAME="platform/ui"
 TAG=$(git rev-parse --verify --short HEAD)
 
-REGISTRY="eris:7750"
+###
+### on eris registry
+###
+# IMAGE_NAME="eris:7750/platform/ui"
+
+###
+### on dockerhub registry
+###
+IMAGE_NAME="jolibrain/platform_ui"
 
 yarn run build
-docker build -t ${REGISTRY}/${IMAGE_NAME}:${TAG} -t ${REGISTRY}/${IMAGE_NAME}:latest .
-docker push ${REGISTRY}/${IMAGE_NAME}
+docker build -t ${IMAGE_NAME}:${TAG} -t ${IMAGE_NAME}:latest .
+docker push ${IMAGE_NAME}
