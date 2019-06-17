3a2aaf5809a671d67d030abf670e212dfc807274
Alexandre Girard <git@alexgirard.com>
2019-06-17 12:01:16 +0200

    fix: add check to avoid error on 500 response from Deepdetect server

--- src/components/widgets/Imaginate/connectors/commons/Description/formats/Simple/index.js
+++ src/components/widgets/Imaginate/connectors/commons/Description/formats/Simple/index.js
@@ -4,9 +4,28 @@ import { observer } from "mobx-react";
 
 @observer
 export default class Simple extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    const { input } = nextProps;
+    return (
+      input &&
+      input.json &&
+      input.json.body &&
+      input.json.body.predictions &&
+      input.json.body.predictions[0]
+    );
+  }
   render() {
     const { input } = this.props;
-    const { classes } = input.json.body.predictions[0];
+    let classes = null;
+    if (
+      input &&
+      input.json &&
+      input.json.body &&
+      input.json.body.predictions &&
+      input.json.body.predictions[0]
+    ) {
+      classes = input.json.body.predictions[0].classes;
+    }
 
     if (!classes) return null;
 
