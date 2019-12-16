ab926f3e7cb5b710a4a91afa6be2f27fc8d5b01a
Alexandre Girard <git@alexgirard.com>
2019-12-16 14:50:08 +0100

    fix: remove comments on docker push commands

--- docker-build.sh
+++ docker-build.sh
@@ -6,5 +6,5 @@ IMAGE_NAME="jolibrain/platform_ui"
 
 npm run build
 docker build -t ${IMAGE_NAME}:latest -t ${IMAGE_NAME}:${TAG} --no-cache .
-#docker push ${IMAGE_NAME}:latest
-#docker push ${IMAGE_NAME}:${TAG}
+docker push ${IMAGE_NAME}:latest
+docker push ${IMAGE_NAME}:${TAG}
