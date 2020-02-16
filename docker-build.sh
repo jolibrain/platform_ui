239ddd15dfa349bf673f17a9ce48128e2117a621
Alexandre Girard <git@alexgirard.com>
2020-02-16 12:41:13 +0100

    build: add git tag in docker build

--- docker-build.sh
+++ docker-build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 set -ex
-TAG=$(git rev-parse --verify --short HEAD)
+TAG=$(git tag --sort=committerdate | tail -1)
+COMMIT=$(git rev-parse --verify --short HEAD)
 IMAGE_NAME="jolibrain/platform_ui"
 
 npm run build
-docker build -t ${IMAGE_NAME}:latest -t ${IMAGE_NAME}:${TAG} --no-cache .
+docker build -t ${IMAGE_NAME}:latest -t ${IMAGE_NAME}:${COMMIT} -t ${IMAGE_NAME}:${TAG} --no-cache .
 docker push ${IMAGE_NAME}:latest
 docker push ${IMAGE_NAME}:${TAG}
