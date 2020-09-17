f890ea04378d6944a6d190828208ede0e9830292
Alexandre Girard <git@alexgirard.com>
2020-09-17 15:07:41 +0200

    chore: remove older docker-build scripts

--- docker-build-18105.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-IMAGE_NAME="jolibrain/platform_ui"
-
-npm run build
-docker build -t ${IMAGE_NAME}:dev_18105 --no-cache .
-#docker push ${IMAGE_NAME}:latest
-#docker push ${IMAGE_NAME}:${TAG}
