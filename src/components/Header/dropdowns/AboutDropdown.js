47f77e11036bafe30f26f721e769e75ad419f7ad
Alexandre Girard <git@alexgirard.com>
2020-12-18 12:22:12 +0100

    fix: update UI with update available notif

--- src/components/Header/dropdowns/AboutDropdown.js
+++ src/components/Header/dropdowns/AboutDropdown.js
@@ -50,52 +50,24 @@ class AboutDropdown extends React.Component {
   render() {
     const { buildInfoStore } = this.props;
 
-    // if (!buildInfoStore.isReady || !buildInfoStore.buildCommitHash) return null;
-
-    let buildInfo = null;
     let updateInfo = null;
 
-    // if (buildInfoStore.isReady) {
-    //   const { buildCommitHash, buildDate, branch } = buildInfoStore;
-
-    //   buildInfo = (
-    //     <div>
-    //       <div className="dropdown-divider" />
-    //       <a
-    //         className="dropdown-item"
-    //         href={`https://gitlab.com/jolibrain/core-ui/commits/${branch}`}
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         Build {buildCommitHash.slice(0, 6)}
-    //         <br />
-    //         {branch !== "master" ? (
-    //           <span>
-    //             Branch <i>{branch}</i>
-    //             <br />
-    //           </span>
-    //         ) : (
-    //           ""
-    //         )}
-    //         updated {moment.unix(buildDate).fromNow()}
-    //       </a>
-    //     </div>
-    //   );
-    // }
-
-    // if (buildInfoStore.isUpdatable) {
-    //   updateInfo = (
-    //     <a
-    //       className="dropdown-item update-link"
-    //       href="https://deepdetect.com/quickstart-platform/#update"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <i className="fas fa-chevron-right" />
-    //       &nbsp; An update is available for your Platform
-    //     </a>
-    //   );
-    // }
+    if (buildInfoStore.isUpdatable) {
+      updateInfo = (
+        <div>
+          <div className="dropdown-divider" />
+          <a
+            className="dropdown-item update-link"
+            href="https://deepdetect.com/quickstart-platform/#update"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span class="badge badge-secondary">{buildInfoStore.latestDockerTag}</span>
+            &nbsp; Update Available
+          </a>
+        </div>
+      );
+    }
 
     return (
       <li
@@ -149,7 +121,6 @@ class AboutDropdown extends React.Component {
           >
             Jolibrain
           </a>
-          {buildInfo}
           {updateInfo}
         </div>
       </li>
