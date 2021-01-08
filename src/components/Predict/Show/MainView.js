c33ba059284794e45edfbccaa0b7f765dd9dac97
Alexandre Girard <git@alexgirard.com>
2021-01-08 11:02:54 +0100

    feat: add available service statistics
    New service statistics where implemented in DeepDetect server: https://github.com/jolibrain/deepdetect/pull/829
    
    These stats are displayed in PredictShow.

--- src/components/Predict/Show/MainView.js
+++ src/components/Predict/Show/MainView.js
@@ -57,6 +57,45 @@ class MainView extends React.Component {
       );
     });
 
+    // prepare service stats display
+    let statsDisplay = null;
+    if(
+      settings.service_stats &&
+        settings.service_stats.predict_success &&
+        settings.service_stats.predict_count &&
+        settings.service_stats.avg_predict_duration &&
+        settings.service_stats.avg_transform_duration &&
+        settings.service_stats.avg_batch_size
+    ) {
+
+      const {
+        predict_success,
+        predict_count,
+        avg_predict_duration,
+        avg_transform_duration,
+        avg_batch_size
+      } = settings.service_stats;
+
+      statsDisplay = (<div className="serviceStats">
+                              <div className="row serviceInfo">
+                                <h4>{parseFloat(avg_batch_size).toFixed(2)}</h4>
+                                <h5>Average Batch Size</h5>
+                              </div>
+                              <div className="row serviceInfo">
+                                <h4>{parseFloat(avg_transform_duration).toFixed(3) * 1000}</h4>
+                                <h5>Average Transform Duration (ms)</h5>
+                              </div>
+                              <div className="row serviceInfo">
+                                <h4>{parseFloat(avg_predict_duration).toFixed(3) * 1000}</h4>
+                                <h5>Average Predict Duration (ms)</h5>
+                              </div>
+                              <div className="row serviceInfo">
+                                <h4>{predict_success} / {predict_count}</h4>
+                                <h5>Predict success / total</h5>
+                              </div>
+                            </div>)
+    }
+
     const repository = modelRepositoriesStore.predictRepositories.find(
       r => r.name === service.name
     );
@@ -79,44 +118,51 @@ class MainView extends React.Component {
               </h3>
               <h4>{settings.description}</h4>
 
-              {settingInfoKeys.map((key, index) => {
+              <div className="row">
+                <div className="col-lg-6 col-md-12">
+                  {settingInfoKeys.map((key, index) => {
 
-                let value = settings[key],
-                    output = "";
+                    let value = settings[key],
+                        output = "";
 
-                if (typeof settings[key] === "object")
-                  value = JSON.stringify(value);
+                    if (typeof settings[key] === "object")
+                      value = JSON.stringify(value);
 
-                if(value.length > 0) {
-                  output = (
-                    <div key={index} className="row serviceInfo">
-                      <h4>{value}</h4>
-                      <h5>{key}</h5>
+                    if(value.length > 0) {
+                      output = (
+                        <div key={index} className="row serviceInfo">
+                          <h4>{value}</h4>
+                          <h5>{key}</h5>
+                        </div>
+                      );
+                    }
+
+                    return output;
+                  })}
+
+                  {repository &&
+                  repository.downloadableFiles &&
+                  repository.downloadableFiles.includes("corresp.txt") ? (
+                    <div className="row serviceCorresp">
+                      <h4>
+                        <a
+                          href={repository.path + "corresp.txt"}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          class names <i className="fas fa-chevron-right" />
+                        </a>
+                      </h4>
+                      <h5>corresp.txt</h5>
                     </div>
-                  );
-                }
-
-                return output;
-              })}
-
-              {repository &&
-              repository.downloadableFiles &&
-              repository.downloadableFiles.includes("corresp.txt") ? (
-                <div className="row serviceCorresp">
-                  <h4>
-                    <a
-                      href={repository.path + "corresp.txt"}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      class names <i className="fas fa-chevron-right" />
-                    </a>
-                  </h4>
-                  <h5>corresp.txt</h5>
+                  ) : (
+                    ""
+                  )}
                 </div>
-              ) : (
-                ""
-              )}
+                <div className="col-lg-6 col-md-12">
+                  { statsDisplay }
+                </div>
+              </div>
             </div>
 
             <div className="col-lg-4 col-md-12">
