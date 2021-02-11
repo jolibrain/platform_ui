2adc1a40b3cd4c4cd1e3fa91aa8d96f36e69c419
Alexandre Girard <git@alexgirard.com>
2021-02-11 11:48:35 +0100

    feat: TrainingShow title is parameterized

--- src/components/widgets/TrainingMonitor/components/MultiTitle/index.js
+++ src/components/widgets/TrainingMonitor/components/MultiTitle/index.js
@@ -13,17 +13,115 @@ class MultiTitle extends React.Component {
       handleRepositoryVisibility
     } = this.props;
 
+    let tableColumns = [
+      {
+        text: "Name",
+        selector: "name",
+        classNameFormatter: (props) => {
+          return `chart-cell-${props.serviceIndex}`
+        }
+      },
+      {
+        text: "Train Loss",
+        selector: "train_loss",
+        isValue: true,
+        formatter: (value, props) => {
+          let content = value;
+          if (value && typeof value.toFixed === "function") {
+            if (value > 1) {
+              content = value.toFixed(3);
+            } else {
+              // Find position of first number after the comma
+              const zeroPosition = value
+                    .toString()
+                    .split("0")
+                    .slice(2)
+                    .findIndex(elem => elem.length > 0);
+
+              content = value.toFixed(zeroPosition + 4);
+            }
+          }
+          return content;
+        }
+      },
+      {
+        text: "Iterations",
+        selector: "iteration",
+        isMeasure: true,
+      },
+      {
+        text: "Iteration (best)",
+        selector: "iteration",
+        isBest: true,
+      }
+    ];
+
+    const firstService = services.length > 0 ?
+          services[0] : null;
+
+    if( firstService.isTimeSeries ) {
+      tableColumns.push({
+        text: "Eucll (best)",
+        selector: "eucll",
+        isBest: true,
+        formatter: (value, props) => {
+          return value !== "--" ? parseFloat(value).toFixed(5) : value;
+        }
+      });
+    } else {
+      tableColumns.push({
+        text: "MAP (best)",
+        selector: "map",
+        isBest: true,
+        formatter: (value, props) => {
+          return value !== "--" ? parseFloat(value).toFixed(5) : value;
+        }
+      });
+    }
+
+    tableColumns = tableColumns.concat([
+      {
+        text: "JSON",
+        formatter: (value, props) => {
+          return <>
+            <a
+              href={`${props.service.path}config.json`}
+              className="badge badge-dark"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              config
+            </a>
+            <br />
+            <a
+              href={`${props.service.path}metrics.json`}
+              className="badge badge-dark"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              metrics
+            </a>
+          </>
+        }
+      },
+      {
+        text: "",
+        formatter: (value, props) => {
+          return <i
+            className={`chart-badge-${props.serviceIndex} fa fa-2x fa-toggle-${
+              props.isVisible ? "off" : "on"
+            }`}
+            onClick={props.handleRepositoryVisibility.bind(this, props.serviceIndex)}
+          />
+        }
+      }
+    ]);
+
     return (
       <table className="title trainingmonitor-multititle">
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Train Loss</th>
-            <th>Iterations</th>
-            <th>Iteration (best)</th>
-            <th>MAP (best)</th>
-            <th>JSON</th>
-            <th></th>
+            { tableColumns.map(col => <th>{col.text}</th>) }
           </tr>
         </thead>
         <tbody>
@@ -35,6 +133,7 @@ class MultiTitle extends React.Component {
                 serviceIndex={index}
                 isVisible={hiddenRepositoriesIndexes.includes(index)}
                 handleRepositoryVisibility={handleRepositoryVisibility}
+                tableColumns={tableColumns}
               />
             );
           })}
