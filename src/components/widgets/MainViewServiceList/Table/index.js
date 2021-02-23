b7cbba294eb51fc98a608b092ce08c69d1e309e1
Alexandre Girard <git@alexgirard.com>
2021-02-23 12:17:47 +0100

    feat: add pagination on TrainingHome table
    Loading all archived jobs created an UI issue with non-responsive
    buttons.
    
    30-items per page pagination has been included in order to avoid this issue.

--- src/components/widgets/MainViewServiceList/Table/index.js
+++ src/components/widgets/MainViewServiceList/Table/index.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { observer } from "mobx-react";
 import { withRouter } from "react-router-dom";
 import moment from "moment"
+import ReactPaginate from 'react-paginate';
 
 import PredictItem from "./Predict";
 import TrainingItem from "./Training";
@@ -17,6 +18,10 @@ class Table extends React.Component {
     super(props);
 
     this.state = {
+      pagination: {
+        offset: 0,
+        perPage: 30
+      },
       columns: [
         {
           text: "",
@@ -192,6 +197,8 @@ class Table extends React.Component {
 
     this.toggleColumn = this.toggleColumn.bind(this);
     this.handleFilterClick = this.handleFilterClick.bind(this)
+    this.handleFilterPreset = this.handleFilterPreset.bind(this);
+    this.handlePageClick = this.handlePageClick.bind(this);
 
     this.formatFloatValue = this.formatFloatValue.bind(this);
     this.bestMinValue = this.bestMinValue.bind(this);
@@ -216,151 +223,199 @@ class Table extends React.Component {
     return Math.max.apply(Math, histValues);
   }
 
-    handleFilterClick(event){
-        const { handlePathFilter } = this.props;
+  handleFilterClick(event){
+    const { pagination } = this.state;
+    const { handlePathFilter } = this.props;
 
-        // missing prop
-        if(!handlePathFilter)
-            return null;
+    // missing prop
+    if(!handlePathFilter)
+      return null;
 
-        handlePathFilter(event.target.innerHTML);
-    }
+    handlePathFilter(event.target.innerHTML);
 
-    toggleColumn(columnText) {
+    this.setState({
+      pagination: {
+        offset: 0,
+        perPage: pagination.perPage
+      }
+    })
+  }
 
-        const newColumns = this.state.columns.map((column, index) => {
-            if(column.text === columnText) {
-                const newColumn = {...column}
-                newColumn.hide = !newColumn.hide
-                return newColumn;
-            }
+  handleFilterPreset(event) {
 
-            return column;
-        })
+  }
 
-        this.setState({
-            ...this.state,
-            columns: newColumns
-        })
+  handlePageClick(data) {
+    const { pagination } = this.state;
+    let selected = data.selected;
+    let offset = Math.ceil(selected * pagination.perPage);
 
-    }
+    this.setState({
+      pagination: {
+        offset: offset,
+        perPage: pagination.perPage
+      }
+    });
+  };
 
-    render() {
+  toggleColumn(columnText) {
 
-        const {
-            services
-        } = this.props;
+    const newColumns = this.state.columns.map((column, index) => {
+      if(column.text === columnText) {
+        const newColumn = {...column}
+        newColumn.hide = !newColumn.hide
+        return newColumn;
+      }
 
-        const tableHeadContent = (
-            <tr>
-              {
-                  this.state.columns
-                      .filter(column => !column.hide)
-                      .map((column, index) =>
-                              <th key={index} scope="col">{column.text}</th>
-                      )
-              }
-            </tr>
+      return column;
+    })
+
+    this.setState({
+      ...this.state,
+      columns: newColumns
+    })
+
+  }
+
+  render() {
+
+    const {
+      services
+    } = this.props;
+
+    const { pagination } = this.state;
+    const pageCount = Math.ceil(services.length / pagination.perPage);
+    const displayedServices = services.slice(
+      pagination.offset,
+      pagination.offset + pagination.perPage
+    )
+
+    const tableHeadContent = (
+      <tr>
+        {
+          this.state.columns
+              .filter(column => !column.hide)
+              .map((column, index) =>
+                   <th key={index} scope="col">{column.text}</th>
+                  )
+        }
+      </tr>
+    );
+
+    const tableBodyContent = displayedServices.map((service, index) => {
+
+      let item = null;
+
+      if (service.isRepository) {
+
+        item = (
+          <ModelRepositoryItem
+            key={index}
+            service={service}
+            columns={this.state.columns}
+            {...this.props}
+          />
         );
 
-        const tableBodyContent = services.map((service, index) => {
+      } else if (service.isDataset) {
 
-            let item = null;
-
-            if (service.isRepository) {
-
-                item = (
-                    <ModelRepositoryItem
-                      key={index}
-                      service={service}
-                      columns={this.state.columns}
-                      {...this.props}
-                    />
-                );
-
-            } else if (service.isDataset) {
-
-                item = (
-                    <DatasetItem
-                      key={index}
-                      dataset={service}
-                      columns={this.state.columns}
-                      {...this.props}
-                    />
-                );
-
-            } else if (
-                service.settings &&
-                    service.settings.training
-            ) {
-
-                item = (
-                    <TrainingItem
-                      key={index}
-                      service={service}
-                      columns={this.state.columns}
-                      {...this.props}
-                    />
-                );
-
-            } else {
-
-                item = (
-                    <PredictItem
-                      key={index}
-                      service={service}
-                      columns={this.state.columns}
-                      {...this.props}
-                    />
-                );
-
-            }
-
-            return item;
-
-        });
-
-        const columnsFilter = (
-            <div className="col column-filters text-right">
-            {
-                this.state.columns
-                    .filter(column => column.toggable)
-                    .map((column, index) => {
-                    return (<span key={index}>
-                          <input
-                            type="checkbox"
-                            checked={!column.hide}
-                            onChange={
-                                this.toggleColumn.bind(this, column.text)
-                            } /> {column.text}
-                            </span>);
-
-                })
-            }
-            </div>
+        item = (
+          <DatasetItem
+            key={index}
+            dataset={service}
+            columns={this.state.columns}
+            {...this.props}
+          />
         );
 
-                return (
-                    <>
-                      {columnsFilter}
-                    <table className="table">
-                    <thead>
-                      { tableHeadContent }
-                    </thead>
-                    <tbody>
-                      { tableBodyContent }
-                    </tbody>
-                    </table>
-                    </>
-                );
+      } else if (
+        service.settings &&
+          service.settings.training
+      ) {
 
-    }
+        item = (
+          <TrainingItem
+            key={index}
+            service={service}
+            columns={this.state.columns}
+            {...this.props}
+          />
+        );
+
+      } else {
+
+        item = (
+          <PredictItem
+            key={index}
+            service={service}
+            columns={this.state.columns}
+            {...this.props}
+          />
+        );
+
+      }
+
+      return item;
+
+    });
+
+    const columnsFilter = (
+      <div className="col column-filters text-right">
+        {
+          this.state.columns
+              .filter(column => column.toggable)
+              .map((column, index) => {
+                return (<span key={index}>
+                                 <input
+                                   type="checkbox"
+                                   checked={!column.hide}
+                                   onChange={
+                                     this.toggleColumn.bind(this, column.text)
+                                   } /> {column.text}
+                   </span>);
+
+              })
+        }
+      </div>
+    );
+
+    return (
+      <>
+        {columnsFilter}
+        <table className="table">
+          <thead>
+            { tableHeadContent }
+          </thead>
+          <tbody>
+            { tableBodyContent }
+          </tbody>
+        </table>
+        <ReactPaginate
+          previousLabel={'previous'}
+          nextLabel={'next'}
+          breakLabel={'...'}
+          breakClassName={'break-me'}
+          pageCount={pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={this.handlePageClick}
+          containerClassName={'pagination justify-content-center'}
+          pageClassName={'page-item'}
+          pageLinkClassName={'page-link'}
+          previousClassName={'page-link'}
+          nextClassName={'page-link'}
+          activeClassName={'active'}
+        />
+      </>
+    );
+
+  }
 
 }
 
 Table.propTypes = {
-    services: PropTypes.array.isRequired,
-    handleCompareStateChange: PropTypes.func,
-    handlePathFilter: PropTypes.func,
+  services: PropTypes.array.isRequired,
+  handleCompareStateChange: PropTypes.func,
+  handlePathFilter: PropTypes.func,
 };
 export default Table;
