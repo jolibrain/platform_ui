98c8fbae9c8af0f5a29d62e7862f9366e37228e5
Alexandre Girard <git@alexgirard.com>
2021-11-08 16:19:25 +0100

    fix: disable showIterTimeScale from current branch

--- src/components/widgets/TrainingMonitor/components/GeneralInfo/index.js
+++ src/components/widgets/TrainingMonitor/components/GeneralInfo/index.js
@@ -88,6 +88,15 @@ class GeneralInfo extends React.Component {
         mltype = service.respInfo.body.mltype;
     }
 
+    const hasMeasureElapsedTime = services.some(service => {
+      const measure_hist = service.jsonMetrics
+        ? service.jsonMetrics.body.measure_hist
+        : service.measure_hist;
+
+      return 'elapsed_time_ms_hist' in measure_hist;
+    })
+
+    // showIterTimeScale={hasMeasureElapsedTime}
     infoCharts.push(
       <MeasureChart
         title="Train Loss"
