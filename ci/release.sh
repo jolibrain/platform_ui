890de738aded2f449e5757aa7adb0c549cc7bcc7
Alexandre Girard <git@alexgirard.com>
2023-11-09 14:37:57 +0100

    build: update tag in public/versio

--- ci/release.sh
+++ ci/release.sh
@@ -11,9 +11,12 @@ if [ -f "CHANGELOG.md" ]; then
   git rm CHANGELOG.md
 fi
 yarn run standard-version -r $kind
-tag=$(cat package.json | jq -r .version)
 
-sed -ne "/^## \[$tag\]/,/^##.*202/p" CHANGELOG.md | sed -e '$d' -e '1d' > note.md
+tag=$(cat package.json | jq -r .version)
+echo v$tag > public/version
+
+sed -ne "/^## \[$tag\]/,/^##.*202/p" CHANGELOG.md \
+    | sed -e '$d' -e '1d' > note.md
 
 cat >> note.md <<EOF
 ### Docker images:
